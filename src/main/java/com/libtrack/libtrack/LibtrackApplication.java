474cfb5d8ae552df7f79f9d26b444df82bf64b04
asishnani7 <asishnani7@gmail.com>
2026-05-12 12:15:18 +0530

    Initial Spring Boot setup with H2 configuration

---
+++ src/main/java/com/libtrack/libtrack/LibtrackApplication.java
@@ -0,0 +1,13 @@
+package com.libtrack.libtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibtrackApplication.class, args);
+	}
+
+}
