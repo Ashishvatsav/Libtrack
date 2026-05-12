4eb1b2744f27ecff7d66031f98dc806c2a4c2bad
asishnani7 <asishnani7@gmail.com>
2026-05-12 12:56:43 +0530

    new changes in code

--- src/main/java/com/libtrack/repository/MemberRepository.java
+++
@@ -1,6 +0,0 @@
-package com.libtrack.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.libtrack.entity.Member;
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
